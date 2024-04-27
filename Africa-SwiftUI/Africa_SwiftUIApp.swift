9a91380dba452effe965890ae716cffe17d7a6ca
yuchormanski <yuchormanski@gmail.com>
2024-04-27 08:40:44 +0300

    Initial Commit

---
+++ Africa-SwiftUI/Africa_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_SwiftUIApp.swift
+//  Africa-SwiftUI
+//
+//  Created by Nikolay  Yuchormanski on 27.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Africa_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
