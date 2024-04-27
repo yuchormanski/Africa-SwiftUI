0689f4ca40a214ff5b680f14b92a24d938092ac1
yuchormanski <yuchormanski@gmail.com>
2024-04-27 10:51:32 +0300

    Created Covers tab view

---
+++ Africa-SwiftUI/View/CoverImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoverImageView.swift
+//  Africa-SwiftUI
+//
+//  Created by Nikolay  Yuchormanski on 27.04.24.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoverImageView()
+}
