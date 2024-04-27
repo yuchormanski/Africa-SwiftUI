e5b10ce5eacaa70b1629380308faa9569c2faa0c
yuchormanski <yuchormanski@gmail.com>
2024-04-27 10:52:09 +0300

    create tab view

--- Africa-SwiftUI/App/Africa_SwiftUIApp.swift
+++ Africa-SwiftUI/App/Africa_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Africa_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- Africa-SwiftUI/View/CoverImageView.swift
+++ Africa-SwiftUI/View/CoverImageView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct CoverImageView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            }//: ForEach LOOP
+            
+        }//: TabView
+        .tabViewStyle(PageTabViewStyle())
+        
+        
     }
 }
 
-#Preview {
-    CoverImageView()
+//MARK: - PREVIEW
+//#Preview {
+//    CoverImageView()
+//}
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
 }
